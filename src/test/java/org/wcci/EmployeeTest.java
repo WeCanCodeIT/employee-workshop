5924a504a9db45056bdefc6adcfbc1d1a8164dfe
Benjamin Williams <ben@wecancodeit.org>
2020-01-22 10:50:48 -0500

    initial commit

---
+++ src/test/java/org/wcci/EmployeeTest.java
@@ -0,0 +1,11 @@
+package org.wcci;
+
+import org.junit.jupiter.api.Test;
+
+public class EmployeeTest {
+    @Test
+    public void shouldHaveName() {
+        Employee underTest = new Employee();
+
+    }
+}
